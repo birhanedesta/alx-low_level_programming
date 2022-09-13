b4c7093d586f6ad3c9fc41adb8f0443459fb9ef2
birhanedesta <birdesta16@gmail.com>
2022-09-13 10:44:47 -0700

    functionsnestedloops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char string[20] = "_putchar\n";
+	int i = 0;
+	while (string[i] != '\0')
+	{
+		_putchar(string[i]);
+		i++;
+	}
+	return (0);
+}
