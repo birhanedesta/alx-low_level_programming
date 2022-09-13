042da73b727ba29186810afd823a70c049868c06
birhanedesta <birdesta16@gmail.com>
2022-09-13 12:39:31 -0700

    functionsnestedloops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,7 @@
  */
 int print_last_digit(int n)
 {
-	int n;
+	int a;
 
 	if (n < 0)
 		n = -n;
@@ -15,5 +15,5 @@ int print_last_digit(int n)
 	if (a < 0)
 		a = -a;
 	_putchar(a + '0');
-	return(a);
+	return (a);
 }
