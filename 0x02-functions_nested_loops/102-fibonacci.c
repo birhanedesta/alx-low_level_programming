bb644f31dd5bba40819073991cd2c78fb8bf31f0
birhanedesta <birdesta16@gmail.com>
2022-09-13 19:55:37 -0700

    functionsnestedloops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/** 
+/**
  * main - prints the first 50 fibonacci numbers starting with 1 and 2
  * followed by a new line
  * Return: Always 0 (Success)
@@ -8,6 +8,7 @@
 int main(void)
 {
 	long int i, j, k, next;
+
 	j = 1;
 	k = 2;
 	for (i = 1; i <= 50; ++i)
