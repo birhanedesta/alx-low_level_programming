07751e7a4bcb76332617f8b1174aea823ba19143
birhanedesta <birdesta16@gmail.com>
2022-09-19 06:32:20 -0700

    strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * puts_half - prints half of a string
- * @str: input string
+ * puts_half - prints half of a string.
+ * @str: input string.
  * Return: no return.
  */
 void puts_half(char *str)
