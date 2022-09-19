5c1fa296c9d416789500472b4954c83b5c47100d
birhanedesta <birdesta16@gmail.com>
2022-09-19 03:32:11 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: input string
+ * Return: no return
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while(*(str = count) != '\0')
+	{
+		_putchar(str[count]);
+		count++;
+	};
+	_putchar('\n');
+}
