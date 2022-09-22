658dd33deff12092d5ce8b25abf4d42079bc25c9
birhanedesta <birdesta16@gmail.com>
2022-09-22 07:39:15 -0700

    arrays

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,6 @@
  * _strcmp - compares two strings
  * @s1: first string to compare
  * @s2: second string to compare
- *
  * Return: less than 0 if s1 is less than s2, 0 if they're equal,
  * more than 0 if s1 is greater than s2
  */
@@ -23,7 +22,6 @@ int _strcmp(char *s1, char *s2)
 }
 /**
  * ALTERNATIVE SOLUTION
- *
  * int _strcmp(char *s1, char *s2)
  * {
  *     int i = 0;
